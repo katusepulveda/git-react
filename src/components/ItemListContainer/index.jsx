00956204cc50cafe54998509e0966779917d5fc6
katusepulveda <kat.s.riquelme@gmail.com>
2022-04-21 01:58:18 -0400

    cambio en ItemListContainer.css

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import  './ItemListContainer.css'
 
 const ItemListContainer = ({saludo}) => {
     return (
-    <div>{saludo}</div>
+    <div className='ItemListContainer'>{saludo}</div>
     )
 }
 
