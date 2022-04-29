13ead56cf2a6cbd9d88f9991528a4e52f271a52d
katusepulveda <kat.s.riquelme@gmail.com>
2022-04-29 01:24:47 -0400

    creación de item.jsx, itemlist.js, array de productos

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemCount from '../ItemCount/ItemCount';
 import './ItemListContainer.css'
+import getData from '../../Services/Data';
 
 const ItemListContainer = ({ saludo }) => {
+    const [products, setProducts] = useState([]);
+    console.log(products)
+
+    useEffect(() =>{
+        getData.then((res) => console.log(res))
+        .catch((err) => console.log(err))
+    
+    }, []);
+
     return (
         <>
             <h2 className='ItemListContainer'>{saludo} 
